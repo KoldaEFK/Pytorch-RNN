2428eaecf603c329c9600528dd0d26d77da1dd30
PetrKolar <kolda.efk@gmail.com>
2021-08-03 09:13:31 +0200

    cleaning, commenting

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ class RNN(nn.Module):
         self.sequence_length = sequence_length
         self.n_layers = n_layers
         self.hidden_size = hidden_size
-        self.rnn = nn.GRU(n_features, hidden_size, n_layers, batch_first=True) #expects input of shape --> batch_size, sequence_length, features
+        self.rnn = nn.GRU(n_features, hidden_size, n_layers, batch_first=True) #expects input of shape --> batch_size, sequence_length, n_features
         self.fc = nn.Linear(hidden_size, output_size) #fully connected layer
     
     def forward(self,x):
@@ -24,40 +24,43 @@ class RNN(nn.Module):
         #do not confuse HIDDEN state with WEIGHT matrix!
         h0 = torch.zeros(self.n_layers,x.size(0),self.hidden_size).to(device) #h0 shape --> n_layers, batch_size, hidden_size
         out, _ = self.rnn(x, h0) #returns hidden states for all time steps
-        out = out[:,-1,:] #I need only the last hidden state (many to one RNN, see README)
+        out = out[:,-1,:] #I need only the last hidden state (many to one RNN, see README.md)
         out = self.fc(out) #out shape --> batch_size, output_size
         return out
     
-    #function predict accpets know_sequence as first input and future_sequence that must DIRECTLY follow the known_sequence
+    #function predict accepts know_sequence as first input and future_sequence that must DIRECTLY follow the known_sequence
     def predict(self,known_sequence, future_sequence):
-        """This is a function for predicting future values for arbitrary number of future time steps"""
+        """function for predicting future values for arbitrary number of future time steps"""
         with torch.no_grad():
             self.eval() #setting the model to evaluation mode and turning off the gradient computation
+
+            if len(known_sequence)<len(self.sequence_length):
+                return "known_sequence must be longer than sequence length"
             
-            known_sequence = known_sequence.to(device) #known_sequence must be longer that sequence_length
+            known_sequence = known_sequence.to(device)
             future_sequence = future_sequence.to(device) #future sequence can be of arbitrary length
             
-            #X is a temporary data that is going to be updated at each future time step
-            x = known_sequence[-self.sequence_length:] #we need only the sequence_length of known data to compute
-            x = torch.reshape(x,(1,-1,11)) #x must be of shape --> batch_size, sequence_length, features = 1, 100, 11
+            #x is going to be updated at each future time step
+            x = known_sequence[-self.sequence_length:]
+            x = torch.reshape(x,(1,-1,11)) #x must be of shape --> batch_size, sequence_length, n_features = 1, 100, 11
             
             outputs = [] #list where outputs of future timesteps will be stored
             
-            for i in range(len(future_sequence)): #future_sequence shape --> future_sequence_length, features
+            for i in range(len(future_sequence)): #future_sequence shape --> future_sequence_length, n_features
                 """calculating outputs and updating the x with the newly predicted target variables"""
-                #more or less repeating forward pass with batch_size = 1
 
+                #more or less repeating forward pass with batch_size = 1
                 h0 = torch.zeros(self.n_layers,1,self.hidden_size).to(device)
                 out, _ = self.rnn(x, h0) #out shape --> 1,batch_size,hidden_size
                 out = out[:,-1,:] #out shape --> 1,hidden_size
-                out = self.fc(out) #out shape --> 1,3
-                out = out[0] #out shape --> 3
+                out = self.fc(out) #out shape --> 1,output_size = 1,3
+                out = out[0] #out shape --> output_size = 3
                 
                 outputs.append(out)
                 
-                #preparing the new data point to be added to x
+                #preparing the new data point new_t to be added to x
                 new_x = future_sequence[i,:8] #known data of the future time step
-                new_t = torch.cat((new_x,out),-1) #creating the new data point by concatenating known 8 features with 3 predicted
+                new_t = torch.cat((new_x,out),-1) #concatenating the 8 known features with 3 predicted
 
                 x = torch.roll(x,-1,1) #shifting the elements in the tensor by -1 (the first element becomes last)
                 x[0,-1] = new_t #replacing the last element with the newly made data point
--- training-testing.py
+++ training-testing.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from model import RNN
+from model import RNN #importing my model from model.py file
 
 #DEVICE CONFIG
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -14,7 +14,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 sequence_length = 100 #number of steps we are looking back
 batch_size = 40
 learning_rate = 0.0001
-input_size = 11 #the number of features
+n_features = 11 #the number of features
 hidden_size = 64 #size of hidden layer in RNN
 output_size = 3 #the targets
 num_epochs = 20
@@ -23,16 +23,16 @@ n_layers = 3
 #DATA
 df = pd.read_csv("train.csv") #train.csv contains 7111 logs
 df_train = df.drop("date_time",1)[:5000] #df_train is shape 5000,11
-scaler = StandardScaler() 
-scaler = scaler.fit(df_train) #computing the parameters for scaling | converts df to numpy array
-df_train = scaler.transform(df_train) 
+scaler = StandardScaler()
+scaler = scaler.fit(df_train) 
+df_train = scaler.transform(df_train) #returns a numpy array
 
-df_test = df.drop("date_time",1)[5000:]
+df_test = df.drop("date_time",1)[5000:] 
 df_test = scaler.transform(df_test)
 
 #RESHAPING the problem from TIME SERIES to SUPERVISED LEARNING
-#X shape --> (length-sequence_length),sequence_length,features 4900,300,11
-#y shape --> (length-sequence_length),targets 4900,11
+#X shape --> (length-sequence_length),sequence_length,n_features = 4900,100,11; one X datapoint shape --> sequence_length,n_features = 100,11
+#y shape --> (length-sequence_length),targets = 4900,11; one y labels shape --> targets = 3
 class Train_Time_Series_Dataset(Dataset):
     """creates a dataset for supervised learning from data series"""
     def __init__(self, complete_data,sequence_length):
@@ -42,7 +42,7 @@ class Train_Time_Series_Dataset(Dataset):
             if end_idx > len(complete_data) - 1:
                 break
                 
-            #seq_x shape --> sequence_length, features (for time steps up to t-1)
+            #seq_x shape --> sequence_length, n_features (for time steps up to t-1)
             #seq_y shape --> targets (for time step t)
             seq_x, seq_y = complete_data[i:end_idx,:], complete_data[end_idx,8:]
             X.append(seq_x)
@@ -61,7 +61,7 @@ class Train_Time_Series_Dataset(Dataset):
     def __len__(self):
         return self.n_samples
 
-#Eval_Dataset is basically used mainly to convert a numpy arrays to tensors and return the data of shape --> "len of input data frame",features
+#Eval_Dataset is basically used mainly to convert a numpy arrays to tensors and return the data of shape --> "len of input data frame",n_features
 #It is not different data than Train_Dataset, only different format used for evaluation not training
 class Eval_Dataset(Dataset):
     """creates a dataset for evaluation"""
@@ -77,14 +77,14 @@ class Eval_Dataset(Dataset):
         return self.n_samples
 
 dataset = Train_Time_Series_Dataset(df_train, sequence_length)
-train_loader = DataLoader(dataset=dataset,batch_size=batch_size,shuffle=True) #train_loader is used to supply data in shuffled BATCHES during training
+train_loader = DataLoader(dataset=dataset,batch_size=batch_size,shuffle=True) #train_loader is used to supply data in randomly shuffled BATCHES during training
 
-eval_dataset = Eval_Dataset(df_train) #used for training
+#eval_dataset = Eval_Dataset(df_train) #used for training
 
 test_dataset = Eval_Dataset(df_test) #not used for training
 
 #MODEL    
-model = RNN(input_size,n_layers,hidden_size,output_size,sequence_length).to(device)
+model = RNN(n_features,n_layers,hidden_size,output_size,sequence_length).to(device)
 
 #loss and optimizer
 criterion = nn.MSELoss()
@@ -99,7 +99,7 @@ model.train() #setting model to training mode
 
 for epoch in range(num_epochs):
     for i, (features,targets) in enumerate(train_loader):
-        #features shape --> batch_size, sequence_length, features
+        #features shape --> batch_size, sequence_length, n_features
         #targets shape --> batch_size, targets
         features = features.to(device)
         targets = targets.to(device)
@@ -115,7 +115,7 @@ for epoch in range(num_epochs):
 
         running_loss+= loss.item() #item returns a number
 
-        if (i+1) % 50 == 0:
+        if (i+1) % 30 == 0:
             print(f'epoch {epoch+1} / {num_epochs}, step {i+1} / {n_total_steps}, loss = {loss.item()}')
             running_loss = 0.0
 
@@ -124,7 +124,7 @@ for epoch in range(num_epochs):
 
 #EVALUATION USING GRAPHS
 #serves for visually assesing the performance of the model
-test_outputs = model.predict(test_dataset[100:600],test_dataset[600:800]) #getting the outputs for data in eval_dataset in second argument of the function predict
+test_outputs = model.predict(test_dataset[100:600],test_dataset[600:800]) #getting the outputs for data in test_dataset in the second argument
 test_outputs = torch.stack(test_outputs)
 test_outputs = test_outputs.cpu().detach().numpy()
 
